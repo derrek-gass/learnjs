14eff48c2ae587af6fe3d3f5f2080f4dec61d955
derrek-gass <dardcore@gmail.com>
2020-02-24 08:41:42 -0800

    working on dist

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -29,16 +29,5 @@ describe('LearnJS', function () {
         $(window).trigger('hashchange');
         expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
     });
-    describe('answer section', function () {
-        it('can check a correct answer by hitting a button', function () {
-            view.find('.answer').val('true');
-            view.find('.check-btn').click();
-            expect(view.find('.result').text()).toEqual('Correct!');
-        });
-        it('rejects an incorrect answer', function () {
-            view.find('.answer').val('false');
-            view.find('.check-btn').click();
-            expect(view.find('.result').text()).toEqual('Incorrect!');
-        });
-    });
+
 });
